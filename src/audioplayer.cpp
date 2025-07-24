f3e1169e1e0050d39813717250001cf6bdc9d2d7
Indective <ibrahemmohamd561@gmail.com>
2025-07-24 15:29:39 +0300

    .

--- src/audioplayer.cpp
+++ src/audioplayer.cpp
@@ -2,12 +2,9 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
-#include "Audio/Sound.hpp"
 
 void audioplayer::play_ruqia()
 {
-    Audio::Sound ruqia { "Audio/ruqia.wav" };
-    ruqia.play();
 }
 
 void audioplayer::play_morning_athkar()
